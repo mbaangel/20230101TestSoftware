569d8992fd6ad741d7efd7d5d189329aebd341a7
mbaangel <95703191+mbaangel@users.noreply.github.com>
2023-07-04 16:06:04 -0600

    first save

--- 01palindrome.js
+++ 01palindrome.js
@@ -1,4 +1,13 @@
 // JavaScript source code
 /*
-- 
+01 Palindrome
+
+- Given a string, check if it is a palindrome or not. You can perform in javascript and with control structures.
+
+01 Example:
+
+isPalindrome("otto") // return: true
+isPalindrome("angel") // return: false
+
 */
+
